227a2805d65dc816435e40f9345ff554549be438
Robin Edser <75099866+fossrob@users.noreply.github.com>
2022-09-21 16:31:00 +0100

    Add comments to initial setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1091
 
+# Create python virtual environment
 python3 -m venv venv
+
+# Activate python virtual environment
 source venv/bin/activate
+
+# Install latest PIP module
 python3 -m pip install --upgrade pip
+
+# Install Ansible and any other Python modules required
 python3 -m pip install -r requirements.txt
+
+# Install Ansible collections required
 ansible-galaxy collection install -r requirements.yml
